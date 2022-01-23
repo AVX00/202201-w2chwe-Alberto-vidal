e36d5aeb7a298f84e4e8991137d3803cce2ee622
Alberto Vidal <93537515+AVX00@users.noreply.github.com>
2022-01-23 17:29:21 +0100

    implemented toggle on click dead/alive

--- site/scripts/dom.js
+++ site/scripts/dom.js
@@ -12,8 +12,8 @@ function getNumberOfCells(board) {
 
 function bornCell(x, y) {
   const cell = document.createElement("td");
-  cell.setAttribute("id", `cell${x}${y}`);
-  cell.setAttribute("class", "cell");
+  cell.setAttribute("id", `cell-${x}-${y}`);
+  cell.setAttribute("class", "cell cell--dead");
   return cell;
 }
 
@@ -26,11 +26,14 @@ function getCellSize(board, numberOfCells) {
   return cellSize;
 }
 
-function toggleCell(cell, cellSize) {
-  cell.setAttribute(
-    "style",
-    `background-color: #000;width: ${cellSize[0]}px; height: ${cellSize[1]}px;`
-  );
+function toggleCell(tableCell, cell) {
+  if (cell.isAlive) {
+    cell.die();
+    tableCell.className = "cell cell--dead";
+  } else {
+    cell.revive();
+    tableCell.className = "cell cell--alive";
+  }
 }
 
 function createCells() {
@@ -38,30 +41,25 @@ function createCells() {
   board.innerHTML = "";
   const chart = document.createElement("table");
   const numberOfCells = getNumberOfCells(board);
+  const cellSize = getCellSize(board, numberOfCells);
+  const village = generateVillage(numberOfCells[1], numberOfCells[0]);
+  greetTheNeighbors(village);
   for (let x = 0; x < numberOfCells[1]; x++) {
     const row = document.createElement("tr");
     for (let y = 0; y < numberOfCells[0]; y++) {
-      row.appendChild(bornCell(x, y));
+      const cell = bornCell(x, y);
+      cell.setAttribute(
+        "style",
+        `width: ${cellSize[0]}px; height: ${cellSize[1]}px;`
+      );
+      cell.addEventListener("mousedown", () => {
+        toggleCell(cell, village[x][y]);
+      });
+      row.appendChild(cell);
     }
     chart.appendChild(row);
   }
   board.appendChild(chart);
-  const cellSize = getCellSize(board, numberOfCells);
-  console.log(cellSize);
-  document.querySelectorAll(".cell").forEach((cell) => {
-    cell.setAttribute(
-      "style",
-      `width: ${cellSize[0]}px; height: ${cellSize[1]}px;`
-    );
-    cell.addEventListener("mousedown", () => {
-      toggleCell(cell, cellSize);
-    });
-  });
-
-  const village = generateVillage(numberOfCells[1], numberOfCells[0]);
-  village[0][0].revive();
-  greetTheNeighbors(village);
-  console.log(village);
 }
 
 createCells();
