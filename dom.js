47a5365d21ceb9078cf10f4ea9db4b0505ce48f1
Alberto Vidal <93537515+AVX00@users.noreply.github.com>
2022-01-23 12:12:24 +0100

    first try with dom, created functions to generate cells

--- dom.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/extensions
-import { generateVillage, greetTheNeighbours } from "./cell.mjs";
-
-const table = document.getElementById("table");
-
-console.log(table, greetTheNeighbours(generateVillage(3, 3)));
